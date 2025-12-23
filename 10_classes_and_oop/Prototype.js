1947612cf7a255e816acbda4a1a62e572fdfd883
Bhavesh Sattavan <bhaveshsattavan@gmail.com>
2025-12-23 14:38:37 +0530

    learn about prototypes

---
+++ 10_classes_and_oop/Prototype.js
@@ -0,0 +1,67 @@
+// let myName = "hitesh     "
+// let mychannel = "chai     "
+
+// console.log(myName.trueLength);
+
+
+let myHeros = ["thor", "spiderman"]
+
+
+let heroPower = {
+    thor: "hammer",
+    spiderman: "sling",
+
+    getSpiderPower: function(){
+        console.log(`Spidy power is ${this.spiderman}`);
+    }
+}
+
+Object.prototype.bhavesh = function(){
+    console.log(`bhavesh is present in all objects`);
+}
+
+Array.prototype.heyBhavesh = function(){
+    console.log(`Bhavesh says hello`);
+}
+
+// heroPower.bhavesh()
+// myHeros.bhavesh()
+// myHeros.heyBhavesh()
+// heroPower.heyBhavesh()
+
+// inheritance
+
+const User = {
+    name: "om",
+    email: "om@google.com"
+}
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment: 'JS assignment',
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+
+Teacher.__proto__ = User
+
+// modern syntax
+Object.setPrototypeOf(TeachingSupport, Teacher)
+
+let anotherUsername = "ChaiAurCode     "
+
+String.prototype.trueLength = function(){
+    console.log(`${this}`);
+    console.log(`True length is: ${this.trim().length}`);
+}
+
+anotherUsername.trueLength()
+"hitesh".trueLength()
+"iceTea".trueLength()
